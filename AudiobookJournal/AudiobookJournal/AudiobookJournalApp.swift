bfdd901e2a87f9a9e273d0ca382994a5367a9bf6
SuhaibDash <suhaib.da.2002@gmail.com>
2026-09-13 21:39:45 -0500

    build: add SwiftUI iOS app scaffold

---
+++ AudiobookJournal/AudiobookJournal/AudiobookJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiobookJournalApp.swift
+//  AudiobookJournal
+//
+//  Created by Suhaib Dashash on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct AudiobookJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudiobookJournal/AudiobookJournalTests/AudiobookJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudiobookJournalTests.swift
+//  AudiobookJournalTests
+//
+//  Created by Suhaib Dashash on 9/13/26.
+//
+
+import Testing
+@testable import AudiobookJournal
+
+struct AudiobookJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
