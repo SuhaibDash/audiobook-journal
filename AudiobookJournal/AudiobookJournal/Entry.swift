c3c7bd0309aa617ac61c991dd471b0bdbe3e1f58
SuhaibDash <suhaib.da.2002@gmail.com>
2026-09-14 19:40:13 -0500

    feat: add story entry model

---
+++ AudiobookJournal/AudiobookJournal/Entry.swift
@@ -0,0 +1,53 @@
+//
+//  Entry.swift
+//  AudiobookJournal
+//
+//  Created by Suhaib Dashash on 9/14/26.
+//
+
+import Foundation
+
+enum EntryType: String, CaseIterable {
+    case note
+    case reflection
+    case prediction
+
+    var displayName: String {
+        switch self {
+        case .note:
+            "Note"
+        case .reflection:
+            "Reflection"
+        case .prediction:
+            "Prediction"
+        }
+    }
+}
+
+struct Entry: Identifiable {
+    let id: UUID
+    let bookID: UUID
+    var type: EntryType
+    var chapterNumber: Int?
+    var body: String
+    let createdAt: Date
+    var updatedAt: Date
+    
+    init(
+        bookID: UUID,
+        type: EntryType,
+        chapterNumber: Int? = nil,
+        body: String,
+        id: UUID = UUID(),
+        createdAt: Date = Date(),
+        updatedAt: Date? = nil
+    ) {
+        self.id = id
+        self.bookID = bookID
+        self.type = type
+        self.chapterNumber = chapterNumber
+        self.body = body
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt ?? createdAt
+    }
+}
