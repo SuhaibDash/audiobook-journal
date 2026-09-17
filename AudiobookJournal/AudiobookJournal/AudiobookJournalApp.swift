2dc6638d142bb888d569dad5992a2a2165950f5e
SuhaibDash <suhaib.da.2002@gmail.com>
2026-09-16 23:31:22 -0500

    feat: inject SwiftData repository at app launch

--- AudiobookJournal/AudiobookJournal/AudiobookJournalApp.swift
+++ AudiobookJournal/AudiobookJournal/AudiobookJournalApp.swift
@@ -9,14 +9,38 @@ import SwiftUI
 import SwiftData
 
 @main
+@MainActor
 struct AudiobookJournalApp: App {
+    private let modelContainer: ModelContainer
+    private let libraryViewModel: LibraryViewModel
+
+    init() {
+        do {
+            let modelContainer = try ModelContainer(
+                for: StoredBook.self,
+                StoredEntry.self
+            )
+
+            self.modelContainer = modelContainer
+
+            let repository = SwiftDataLibraryRepository(
+                modelContext: modelContainer.mainContext
+            )
+
+            libraryViewModel = LibraryViewModel(
+                repository: repository
+            )
+        } catch {
+            fatalError(
+                "Unable to create SwiftData storage: \(error.localizedDescription)"
+            )
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: libraryViewModel)
         }
-        .modelContainer(for: [
-            StoredBook.self,
-            StoredEntry.self
-        ])
+        .modelContainer(modelContainer)
     }
 }
