3be1caccf8456dfd08f9efff9f1a93162d398125
SuhaibDash <suhaib.da.2002@gmail.com>
2026-09-18 00:53:49 -0500

    test: add failing library repository double

--- AudiobookJournal/AudiobookJournalTests/AudiobookJournalTests.swift
+++ AudiobookJournal/AudiobookJournalTests/AudiobookJournalTests.swift
@@ -5,8 +5,32 @@
 //  Created by Suhaib Dashash on 9/13/26.
 //
 
+import Foundation
 import Testing
 @testable import AudiobookJournal
+
+private enum TestRepositoryError: LocalizedError {
+    case saveFailed
+
+    var errorDescription: String? {
+        "The test repository refused to save."
+    }
+}
+
+@MainActor
+private final class FailingLibraryRepository: LibraryRepository {
+    func fetchLibrary() throws -> LibrarySnapshot {
+        .empty
+    }
+
+    func save(book: Book) throws {
+        throw TestRepositoryError.saveFailed
+    }
+
+    func save(entry: Entry) throws {
+        throw TestRepositoryError.saveFailed
+    }
+}
 
 struct AudiobookJournalTests {
 
